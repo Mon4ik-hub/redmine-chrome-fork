71c90db99b0bc52ae135e950909d721c09a33817
Denis Monakhov <mon4ik@vk.com>
2026-09-26 00:36:54 +0300

    Add fork copyright to the options footer and LICENSE

--- src/components/options/Footer.vue
+++ src/components/options/Footer.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="footer mt100 tc">
-    &copy; <a href="https://github.com/wenzhixin/">https://github.com/wenzhixin</a> 2014-2030
+    &copy; <a href="https://github.com/wenzhixin/">https://github.com/wenzhixin</a> 2014-2030<br>
+    Fork &copy; 2026 <a href="https://github.com/Mon4ik-hub/">Mon4ik-hub</a>
   </div>
 </template>
